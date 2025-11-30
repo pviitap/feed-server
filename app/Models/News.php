fa0a27319ae3c691e3f13578db23bf42e7540a86
viitap <viitap>
2025-11-30 13:58:39 +0200

    Add teletext as news source

--- app/Models/News.php
+++ app/Models/News.php
@@ -18,23 +18,54 @@ class News extends Model
         'link'
     ];
 
-    /**
-     * @throws ConnectionException
-     */
-    public static function updateDBFromSources(): array
-    {
-        $response = Http::get('https://hn.algolia.com/api/v1/search?tags=front_page');
-
-        $hnFrontPageItems = $response->json()['hits'];
-        $filteredNews = array_filter($hnFrontPageItems, fn ($item) => $item['num_comments'] > 100);
-
-        $upsertData = array_map(fn ($item) => [
-            'description' => $item['title'] . " (" . $item['num_comments'] . " comments)",
-            'source' => 'hackernews',
-            'link' => 'https://news.ycombinator.com/item?id=' . $item['story_id']
-        ] , $filteredNews);
-        News::upsert($upsertData, uniqueBy: ['link'], update: ['description']);
-
-        return $upsertData;
+    public static function getNewsForToday() {
+        $news = self::where('created_at', '>=', now()->subDays(1))->get();
+        if ($news->isEmpty()) {
+            self::updateDBFromSources();
+            $news = self::where('created_at', '>=', now()->subDays(1))->get();
+        }
+        return $news;
     }
+
+    public static function updateDBFromSources(): void
+    {
+        self::updateHNNews();
+
+        if (env('TELETEXT_api')) {
+            self::updateTeletextNews();
+        }
+    }
+
+    private static function updateTeletextNews(): void {
+        try {
+            $teletextResponse = Http::get(env('TELETEXT_api') .'?app_id=' .env('TELETEXT_app_id') . '&app_key=' .env('TELETEXT_app_key'));
+            $teletextResponseJson = $teletextResponse->json();
+            $items = $teletextResponseJson['teletext']['page']['subpage'][0]['content'][1]['line'];
+            $filteredNews = array_filter($items, fn($item) => key_exists('Text', $item) &&  str_starts_with($item['Text'], '{DH}'));
+            $upsertData = array_map(fn($item) => [
+                'description' => str_replace('{DH}', '', $item['Text']),
+                'source' => 'teletext'
+            ], $filteredNews);
+            News::upsert($upsertData, uniqueBy: ['link'], update: ['description']);
+        } catch (ConnectionException $e) {
+            \Illuminate\Log\log('Failed to fetch teletext news');
+        }
+    }
+
+    private static function updateHNNews() {
+        try {
+            $hnFrontPageResponse = Http::get('https://hn.algolia.com/api/v1/search?tags=front_page');
+            $hnFrontPageJson = $hnFrontPageResponse->json();
+            $filteredNews = array_filter($hnFrontPageJson['hits'], fn ($item) => $item['num_comments'] > 100);
+            $upsertData = array_map(fn ($item) => [
+                'description' => $item['title'] . " (" . $item['num_comments'] . " comments)",
+                'source' => 'hackernews',
+                'link' => 'https://news.ycombinator.com/item?id=' . $item['story_id']
+            ] , $filteredNews);
+            News::upsert($upsertData, uniqueBy: ['link'], update: ['description']);
+        } catch (ConnectionException $e) {
+            \Illuminate\Log\log('Failed to fetch hn news');
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Models\News;
 
 Route::get('/', function () {
-    $news = News::where('created_at', '>=', now()->subDays(1))->get();
-    if ($news->isEmpty()) {
-        $news = News::updateDBFromSources();
-    }
-    return view('index', ['news' => $news]);
+    return view('index', ['news' => News::getNewsForToday()]);
 });
